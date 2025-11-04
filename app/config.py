748b0b1b9f29f62ab4714c5632f24aa16b31f39e
RayBoyNet <raynaldo_911@hotmail.com>
2025-11-05 06:51:37 +0800

    removed render.yaml and edited db_url

--- app/config.py
+++ app/config.py
@@ -4,11 +4,12 @@ class Settings(BaseSettings):
     algorithm: str 
     secret_key: str 
     access_token_exp_minutes: int 
-    db_username: str 
-    db_password: str 
-    db_hostname: str 
-    db_port: str 
-    db_name: str 
+    # db_username: str 
+    # db_password: str 
+    # db_hostname: str 
+    # db_port: str 
+    # db_name: str 
+    db_url: str
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     
     
--- app/database.py
+++ app/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker,declarative_base
 from .config import settings
 
 # DATABASE_URL = "postgresql://postgres:raymond@localhost/testdb"
-DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_password}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}"
-engine = create_engine(DATABASE_URL)
+# DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_password}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}"
+engine = create_engine(settings.db_url)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
